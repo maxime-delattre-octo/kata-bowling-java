688a23e3eb77d39e97f068efdf760b6d08173c77
20010115 <francois.olivier@adeo.com>
2021-03-10 17:38:46 +0100

    add strike case

--- src/main/java/org/example/Partie.java
+++ src/main/java/org/example/Partie.java
@@ -1,15 +1,38 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Partie {
 
-    public Integer calculScore(List<Integer> lancers){
+    public Integer calculScore(Map<Integer,List<Integer>> frames){
         Integer score = 0;
-        for(Integer lancer:lancers){
-            score+=lancer;
+        List<Map.Entry<Integer,List<Integer>>> frameList = getFramesAsList(frames);
+
+        for(int i=0; i < frameList.size(); i++){
+            if(frameList.get(i).getValue().size()==1){
+                score += 10;
+                for(Integer lancer : frameList.get(i+1).getValue()){
+                    score += lancer;
+                }
+            }else{
+                for(Integer lancer : frameList.get(i).getValue()){
+                    score += lancer;
+                }
+            }
         }
+
         return score;
+    }
+
+    private List<Map.Entry<Integer,List<Integer>>> getFramesAsList(Map<Integer,List<Integer>> frames){
+        List<Map.Entry<Integer,List<Integer>>>entries = new ArrayList<>();
+        for(Map.Entry entry: frames.entrySet()){
+            entries.add(entry);
+        }
+        return entries;
     }
 
 }
--- src/test/java/org/example/PartieTest.java
+++ src/test/java/org/example/PartieTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,13 +13,69 @@ public class PartieTest {
 
     @Test
     public void scoreEquals0WhenAllFail(){
-        assertEquals(0,partie.calculScore(List.of(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)));
+        assertEquals(0,partie.calculScore(
+                Map.of(
+                        1, List.of(0,0),
+                        2, List.of(0,0),
+                        3, List.of(0,0),
+                        4, List.of(0,0),
+                        5, List.of(0,0),
+                        6, List.of(0,0),
+                        7, List.of(0,0),
+                        8, List.of(0,0),
+                        9, List.of(0,0),
+                        10, List.of(0,0)
+                )));
     }
 
     @Test
     public void scoreEqualsSumOfThrowWhenThereIsNoStrikeNorSpare(){
-        assertEquals(1,partie.calculScore(List.of(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)));
+        assertEquals(1,partie.calculScore(
+                Map.of(
+                        1, List.of(1,0),
+                        2, List.of(0,0),
+                        3, List.of(0,0),
+                        4, List.of(0,0),
+                        5, List.of(0,0),
+                        6, List.of(0,0),
+                        7, List.of(0,0),
+                        8, List.of(0,0),
+                        9, List.of(0,0),
+                        10, List.of(0,0)
+                )));
     }
 
-    
+    @Test
+    public void scoreWithOneStrikeNoSpare(){
+        assertEquals(18,partie.calculScore(
+                Map.of(
+                        1, List.of(10),
+                        2, List.of(3,1),
+                        3, List.of(0,0),
+                        4, List.of(0,0),
+                        5, List.of(0,0),
+                        6, List.of(0,0),
+                        7, List.of(0,0),
+                        8, List.of(0,0),
+                        9, List.of(0,0),
+                        10, List.of(0,0)
+                )));
+    }
+
+    @Test
+    public void scoreWithOneSpareNoStrike(){
+        assertEquals(12,partie.calculScore(
+                Map.of(
+                        1, List.of(5,5),
+                        2, List.of(1,0),
+                        3, List.of(0,0),
+                        4, List.of(0,0),
+                        5, List.of(0,0),
+                        6, List.of(0,0),
+                        7, List.of(0,0),
+                        8, List.of(0,0),
+                        9, List.of(0,0),
+                        10, List.of(0,0)
+                )));
+    }
 }
