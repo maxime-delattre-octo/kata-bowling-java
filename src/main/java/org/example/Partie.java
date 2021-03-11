dc44ec2fd731f5ad7dcdbacfda0f3989209e96cd
fgeneste <f.geneste@senat.fr>
2021-03-11 11:10:08 +0100

    All tests ok

--- src/main/java/org/example/Partie.java
+++ src/main/java/org/example/Partie.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -12,14 +13,16 @@ public class Partie {
         List<Map.Entry<Integer,List<Integer>>> frameList = getFramesAsList(frames);
 
         for(int i=0; i < frameList.size(); i++){
-            if(frameList.get(i).getValue().size()==1){
+            if(isStrike(frameList.get(i))){
                 score += 10;
-                for(Integer lancer : frameList.get(i+1).getValue()){
-                    score += lancer;
+                score += calculateStrikeBonus(frameList, i);
+            } else{
+                if(scoreOfFrame(frameList.get(i))==10){
+                    score += 10;
+                    score += frameList.get(i+1).getValue().get(0);
                 }
-            }else{
-                for(Integer lancer : frameList.get(i).getValue()){
-                    score += lancer;
+                else {
+                    score += scoreOfFrame(frameList.get(i));
                 }
             }
         }
@@ -32,7 +35,43 @@ public class Partie {
         for(Map.Entry entry: frames.entrySet()){
             entries.add(entry);
         }
+        entries = entries.stream().sorted(Map.Entry.comparingByKey()).collect(Collectors.toList());
         return entries;
+    }
+
+    private Integer scoreOfFrame(Map.Entry<Integer,List<Integer>> frame){
+        Integer score = 0;
+        for(Integer lancer : frame.getValue()){
+            score += lancer;
+        }
+        return score;
+    }
+
+    private boolean isStrike(Map.Entry<Integer,List<Integer>> frame){
+        if(frame.getValue().get(0)==10)
+            return true;
+        return false;
+    }
+
+    private Integer calculateStrikeBonus(List<Map.Entry<Integer,List<Integer>>> frameList, Integer currentFrameIndex){
+        Integer bonus = 0;
+        if(currentFrameIndex<frameList.size()-2){
+            if(isStrike(frameList.get(currentFrameIndex+1))) {
+                bonus += 10 + frameList.get(currentFrameIndex + 2).getValue().get(0);
+            }
+            else {
+                bonus += scoreOfFrame(frameList.get(currentFrameIndex + 1));
+            }
+        }
+        else
+            if(currentFrameIndex<frameList.size()-1) {
+                bonus += frameList.get(currentFrameIndex + 1).getValue().get(0) + frameList.get(currentFrameIndex + 1).getValue().get(1);
+            }
+            else {
+                bonus += frameList.get(currentFrameIndex).getValue().get(1) + frameList.get(currentFrameIndex).getValue().get(2);
+            }
+
+        return bonus;
     }
 
 }
--- src/test/java/org/example/PartieTest.java
+++ src/test/java/org/example/PartieTest.java
@@ -78,4 +78,85 @@ public class PartieTest {
                         10, List.of(0,0)
                 )));
     }
+
+    @Test
+    public void scoreWithTwoSpareConsecutivelyNoStrike(){
+        assertEquals(27,partie.calculScore(
+                Map.of(
+                        1, List.of(5,5),
+                        2, List.of(5,5),
+                        3, List.of(1,0),
+                        4, List.of(0,0),
+                        5, List.of(0,0),
+                        6, List.of(0,0),
+                        7, List.of(0,0),
+                        8, List.of(0,0),
+                        9, List.of(0,0),
+                        10, List.of(0,0)
+                )));
+    }
+
+    @Test
+    public void scoreWithTwoOrMoreStrikeConsecutivelyNoStrike(){
+        assertEquals(33,partie.calculScore(
+                Map.of(
+                        1, List.of(10),
+                        2, List.of(10),
+                        3, List.of(1,0),
+                        4, List.of(0,0),
+                        5, List.of(0,0),
+                        6, List.of(0,0),
+                        7, List.of(0,0),
+                        8, List.of(0,0),
+                        9, List.of(0,0),
+                        10, List.of(0,0)
+                )));
+        assertEquals(93,partie.calculScore(
+                Map.of(
+                        1, List.of(10),
+                        2, List.of(10),
+                        3, List.of(10),
+                        4, List.of(10),
+                        5, List.of(1,0),
+                        6, List.of(0,0),
+                        7, List.of(0,0),
+                        8, List.of(0,0),
+                        9, List.of(0,0),
+                        10, List.of(0,0)
+                )));
+    }
+
+    @Test
+    public void scoreWithSpareOnLastFrame(){
+        assertEquals(42,partie.calculScore(
+                Map.of(
+                        1, List.of(5,5),
+                        2, List.of(5,5),
+                        3, List.of(1,0),
+                        4, List.of(0,0),
+                        5, List.of(0,0),
+                        6, List.of(0,0),
+                        7, List.of(0,0),
+                        8, List.of(0,0),
+                        9, List.of(0,0),
+                        10, List.of(5,5,5)
+                )));
+    }
+
+    @Test
+    public void scoreWithPerfectGame(){
+        assertEquals(300,partie.calculScore(
+                Map.of(
+                        1, List.of(10),
+                        2, List.of(10),
+                        3, List.of(10),
+                        4, List.of(10),
+                        5, List.of(10),
+                        6, List.of(10),
+                        7, List.of(10),
+                        8, List.of(10),
+                        9, List.of(10),
+                        10, List.of(10,10,10)
+                )));
+    }
 }
