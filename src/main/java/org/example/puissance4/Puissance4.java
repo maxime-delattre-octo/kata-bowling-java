2b95818b873b92ebcf817bd65b9eb050809cfd20
fgeneste <f.geneste@senat.fr>
2021-03-12 11:42:21 +0100

    All tests ok

--- src/main/java/org/example/puissance4/Puissance4.java
+++ src/main/java/org/example/puissance4/Puissance4.java
@@ -1,5 +1,7 @@
 package org.example.puissance4;
 
+import org.example.ScoreConsoleDisplayer;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -7,24 +9,126 @@ import java.util.stream.Collectors;
 public class Puissance4 {
 
     List<List<String>> grille;
+    String dernierJetonJoue;
 
     public Puissance4(){
+
+        initGrille();
+    }
+
+    public void vider(){
+        initGrille();
+    }
+
+    private void initGrille(){
         grille = new ArrayList();
-        for(int i=0;i<6;i++){
+        for(int i=0;i<6;i++) {
             List<String> ligne = new ArrayList<>();
-            for(int j = 0;j<7;j++){
+            for (int j = 0; j < 7; j++) {
                 ligne.add(".");
             }
             grille.add(ligne);
         }
     }
 
-    public void ajouterJeton(Integer colonne, String joueur){
+    public boolean ajouterJeton(Integer colonne, String joueur){
+        boolean coup_autorise = true;
+        if(joueur.equals(dernierJetonJoue)) {
+            return false;
+        }
+        else {
+            dernierJetonJoue = joueur;
+        }
+
+        if(isFinished()) {
+            return false;
+        }
+
         int ligne = 5;
         while(!grille.get(ligne).get(colonne-1).equals(".") && ligne>=0){
             ligne--;
         }
         grille.get(ligne).set(colonne-1,joueur);
+        return coup_autorise;
+    }
+
+    public boolean isFinished() {
+        int colonne = 0;
+        int cpt = 0;
+        String old_value = ".";
+        //Test la victoire verticale
+        for(int ligne=5; ligne>=0; ligne--){
+            String value = grille.get(ligne).get(colonne);
+            if(value.equals(old_value) && !old_value.equals(".")){
+                cpt++;
+            }
+            else {
+                cpt=1;
+                old_value=value;
+            }
+
+            if(cpt==4)
+                return true;
+        }
+        cpt = 0;
+        old_value = ".";
+        //Test la victoire horizontale
+        for(int ligne=5; ligne>=0; ligne--){
+            List<String> line = grille.get(ligne);
+            for(int col=0; col<7; col++){
+                String value =line.get(col);
+                if(value.equals(old_value) && !old_value.equals(".")){
+                    cpt++;
+                }
+                else {
+                    cpt=1;
+                    old_value=value;
+                }
+
+                if(cpt==4)
+                    return true;
+            }
+        }
+
+        //Test la victoire diagonale montante droite
+        for(int ligne=5; ligne>=3; ligne--){
+            List<String> line = grille.get(ligne);
+            for(int col=0; col<=3; col++){
+                String value =line.get(col);
+                if(!value.equals(".")){
+                    boolean ok = true;
+                    for(int i=1; i<4; i++){
+                        if(!grille.get(ligne-i).get(col+i).equals(value)) {
+                            ok = false;
+                        }
+                    }
+                    if(ok) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        //Test la victoire diagonale montante gauche
+        for(int ligne=5; ligne>=3; ligne--){
+            List<String> line = grille.get(ligne);
+            for(int col=6; col>=3; col--){
+                String value =line.get(col);
+                if(!value.equals(".")){
+                    boolean ok = true;
+                    for(int i=1; i<4; i++){
+                        if(!grille.get(ligne-i).get(col-i).equals(value)) {
+                            ok = false;
+                        }
+                    }
+                    if(ok) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 
     public String toString(){
--- src/test/java/org/example/puissance4/Puissance4Test.java
+++ src/test/java/org/example/puissance4/Puissance4Test.java
@@ -1,9 +1,14 @@
 package org.example.puissance4;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.atLeast;
 
+@ExtendWith(MockitoExtension.class)
 public class Puissance4Test {
 
     @Test
@@ -61,6 +66,160 @@ public class Puissance4Test {
         puissance4.ajouterJeton(1,"X");
         puissance4.ajouterJeton(1,"O");
         puissance4.ajouterJeton(1,"X");
+        assertEquals(expectedDisplay,puissance4.toString());
+    }
+
+    @Test
+    public void JaiGagneSiJaligne4JetonsVerticalement(){
+        String expectedDisplay = ". . . . . . .\n"
+                +". . . . . . .\n"
+                +"X . . . . . .\n"
+                +"X . . . . . .\n"
+                +"X . . . . . .\n"
+                +"X . . . . . .\n";
+
+        Puissance4 puissance4 = new Puissance4();
+        puissance4.ajouterJeton(1,"X");
+        puissance4.ajouterJeton(2,"O");
+        puissance4.ajouterJeton(1,"X");
+        puissance4.ajouterJeton(2,"O");
+        puissance4.ajouterJeton(1,"X");
+        puissance4.ajouterJeton(2,"O");
+        puissance4.ajouterJeton(1,"X");
+        assertTrue(puissance4.isFinished());
+        //assertEquals(expectedDisplay,puissance4.toString());
+        //Mockito.verify(puissance4VictoryTestor, atLeast(1)).isFinished(puissance4.grille);
+    }
+
+    @Test
+    public void JaiGagneSiJaligne4JetonsHorizontalement(){
+        String expectedDisplay = ". . . . . . .\n"
+                +". . . . . . .\n"
+                +". . . . . . .\n"
+                +". . . . . . .\n"
+                +". . . . . . .\n"
+                +"X X X X . . .\n";
+
+        Puissance4 puissance4 = new Puissance4();
+        puissance4.ajouterJeton(1,"X");
+        puissance4.ajouterJeton(1,"O");
+        puissance4.ajouterJeton(2,"X");
+        puissance4.ajouterJeton(2,"O");
+        puissance4.ajouterJeton(3,"X");
+        puissance4.ajouterJeton(3,"O");
+        puissance4.ajouterJeton(4,"X");
+        assertTrue(puissance4.isFinished());
+        //assertEquals(expectedDisplay,puissance4.toString());
+        //Mockito.verify(puissance4VictoryTestor, atLeast(1)).isFinished(puissance4.grille);
+    }
+
+    @Test
+    public void JaiGagneSiJaligne4JetonsEnDiagonale(){
+        String expectedDisplay = ". . . . . . .\n"
+                +". . . . . . .\n"
+                +". . . X . . .\n"
+                +". . X X . . .\n"
+                +"O X X O . . .\n"
+                +"X O O O . . .\n";
+
+        Puissance4 puissance4 = new Puissance4();
+        puissance4.ajouterJeton(1,"X");
+        puissance4.ajouterJeton(2,"O");
+        puissance4.ajouterJeton(2,"X");
+        puissance4.ajouterJeton(3,"O");
+        puissance4.ajouterJeton(3,"X");
+        puissance4.ajouterJeton(4,"O");
+        puissance4.ajouterJeton(3,"X");
+        puissance4.ajouterJeton(4,"O");
+        puissance4.ajouterJeton(4,"X");
+        puissance4.ajouterJeton(1,"O");
+        puissance4.ajouterJeton(4,"X");
+        assertTrue(puissance4.isFinished());
+        //assertEquals(expectedDisplay,puissance4.toString());
+    }
+
+    @Test
+    public void JaiGagneSiJaligne4JetonsEnDiagonaleGauche(){
+        String expectedDisplay = ". . . . . . .\n"
+                +". . . . . . .\n"
+                +". . . X . . .\n"
+                +". . . X X . .\n"
+                +". . . O X X O\n"
+                +". . . O O O X\n";
+
+        Puissance4 puissance4 = new Puissance4();
+        puissance4.ajouterJeton(7,"X");
+        puissance4.ajouterJeton(6,"O");
+        puissance4.ajouterJeton(6,"X");
+        puissance4.ajouterJeton(5,"O");
+        puissance4.ajouterJeton(5,"X");
+        puissance4.ajouterJeton(4,"O");
+        puissance4.ajouterJeton(5,"X");
+        puissance4.ajouterJeton(4,"O");
+        puissance4.ajouterJeton(4,"X");
+        puissance4.ajouterJeton(7,"O");
+        puissance4.ajouterJeton(4,"X");
+        assertTrue(puissance4.isFinished());
+        //assertEquals(expectedDisplay,puissance4.toString());
+    }
+
+    @Test
+    public void ImpossibleDAjouterUnJetonDansUneColonnePleine(){
+        Puissance4 puissance4 = new Puissance4();
+        puissance4.ajouterJeton(1,"X");
+        puissance4.ajouterJeton(1,"O");
+        puissance4.ajouterJeton(1,"X");
+        puissance4.ajouterJeton(1,"O");
+        puissance4.ajouterJeton(1,"X");
+        puissance4.ajouterJeton(1,"O");
+        assertThrows(IndexOutOfBoundsException.class, () -> {puissance4.ajouterJeton(1,"X");});
+    }
+
+    @Test
+    public void ImpossibleDeJouer2FoisDeSuite(){
+        Puissance4 puissance4 = new Puissance4();
+        puissance4.ajouterJeton(1,"X");
+        assertFalse(puissance4.ajouterJeton(1,"X"));
+    }
+
+    @Test
+    public void QuandPartieTermineeImpossibleDeJouer(){
+        Puissance4 puissance4 = new Puissance4();
+        puissance4.ajouterJeton(7,"X");
+        puissance4.ajouterJeton(6,"O");
+        puissance4.ajouterJeton(6,"X");
+        puissance4.ajouterJeton(5,"O");
+        puissance4.ajouterJeton(5,"X");
+        puissance4.ajouterJeton(4,"O");
+        puissance4.ajouterJeton(5,"X");
+        puissance4.ajouterJeton(4,"O");
+        puissance4.ajouterJeton(4,"X");
+        puissance4.ajouterJeton(7,"O");
+        puissance4.ajouterJeton(4,"X");
+        assertFalse(puissance4.ajouterJeton(1,"O"));
+    }
+
+    @Test
+    public void LaGrilleSeVide(){
+        String expectedDisplay = ". . . . . . .\n"
+                +". . . . . . .\n"
+                +". . . . . . .\n"
+                +". . . . . . .\n"
+                +". . . . . . .\n"
+                +". . . . . . .\n";
+        Puissance4 puissance4 = new Puissance4();
+        puissance4.ajouterJeton(7,"X");
+        puissance4.ajouterJeton(6,"O");
+        puissance4.ajouterJeton(6,"X");
+        puissance4.ajouterJeton(5,"O");
+        puissance4.ajouterJeton(5,"X");
+        puissance4.ajouterJeton(4,"O");
+        puissance4.ajouterJeton(5,"X");
+        puissance4.ajouterJeton(4,"O");
+        puissance4.ajouterJeton(4,"X");
+        puissance4.ajouterJeton(7,"O");
+        puissance4.ajouterJeton(4,"X");
+        puissance4.vider();
         assertEquals(expectedDisplay,puissance4.toString());
     }
 
